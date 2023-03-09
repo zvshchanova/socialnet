aef14a00dde3bb016c0f6359d96d5fe1a91ce264
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2023-03-09 20:54:09 +0200

    51 i axios Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,32 @@
+import axios from "axios";
 import React from "react";
-import styles from './users.module.css'
+import styles from './users.module.css';
+import userPhoto from '../../assets/images/user.png'
 
 const Users = (props) =>{
  if(props.users.length === 0){
-// props.setUsers( 
-//     //  users: 
-//     [
-//     {id: 1, photoUrl: '',followed: true, fullname: 'Roman Orekhovskiy', status: "I am happy", location: {city: "Stokholm", counntry: "Sweden"}},
-//     {id: 2, photoUrl: '',followed: false, fullname: 'Sophia Bnivier', status: "I am boss", location: {city: "Kiev", counntry: "Ukraine"}},
-//     {id: 3, photoUrl: '',followed: true, fullname: 'Alex Gorbatovskiy', status: "I am a student", location: {city: "Warshava", Poland: "Sweden"}}
-//     ]  
-//  )
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+    .then(responce => {
+    props.setUsers(responce.data.items)
+    }
+ )
  }
 
     return (<div>
         {
             props.users.map( u => 
             <div key={u.id}>
-                <div>🌞<img src={u.photoUrl} className={styles.userPhoto}/></div>
+                <div><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/></div>
                 <div>{u.followed 
                 ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
                 : <button onClick={() => {props.follow(u.id)}}>Follow</button>
                 }</div>
 
-                <div>{u.fullname}</div>            
+                <div>{u.name}</div>           
 
                 <div>{u.status}</div>
-                <div>{u.location.city}</div> 
-                <div>{u.location.counntry}</div>
+                <div>{"u.location.city"}</div> 
+                <div>{"u.location.counntry"}</div>
             </div>
             )
         }
