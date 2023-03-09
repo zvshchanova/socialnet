917b3ff84e56207661a74e3169a7ba6bbef5868d
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2023-03-09 21:43:08 +0200

    53 extends React.Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,35 +3,39 @@ import React from "react";
 import styles from './users.module.css';
 import userPhoto from '../../assets/images/user.png'
 
-const Users = (props) =>{
- if(props.users.length === 0){
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-    .then(responce => {
-    props.setUsers(responce.data.items)
-    }
- )
- }
-
-    return (<div>
-        {
-            props.users.map( u => 
-            <div key={u.id}>
-                <div><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/></div>
-                <div>{u.followed 
-                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                : <button onClick={() => {props.follow(u.id)}}>Follow</button>
-                }</div>
-
-                <div>{u.name}</div>           
-
-                <div>{u.status}</div>
-                <div>{"u.location.city"}</div> 
-                <div>{"u.location.counntry"}</div>
+class Users extends React.Component {
+    getUsers = () => {
+        if(this.props.users.length === 0){
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(responce => {
+            this.props.setUsers(responce.data.items)
+            }
+         )
+    }}
+    render() {
+        return (<div>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+                this.props.users.map( u => 
+                <div key={u.id}>
+                    <div><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/></div>
+                    <div>{u.followed 
+                    ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
+                    : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>
+                    }</div>
+    
+                    <div>{u.name}</div>          
+    
+                    <div>{u.status}</div>
+                    <div>{"u.location.city"}</div> 
+                    <div>{"u.location.counntry"}</div>
+                </div>
+                )
+            }
             </div>
-            )
-        }
-        </div>
-    )
-};
+        )
+    }
+
+}
 
 export default Users
